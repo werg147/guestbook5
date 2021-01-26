16352e6e3324c517e24fa4d0962f800a01a84a45
장세림 <@DESKTOP-1CIRGN6>
2021-01-26 17:17:09 +0900

    +delete 수정

--- src/main/java/com/javaex/controller/GuestController.java
+++ src/main/java/com/javaex/controller/GuestController.java
@@ -61,20 +61,21 @@ public class GuestController {
 	
 	//삭제
 	@RequestMapping(value="/delete", method= {RequestMethod.GET, RequestMethod.POST})
-	public String delete(@ModelAttribute GuestVo guestVo, Model model) {
+	public String delete(@ModelAttribute GuestVo guestVo) {
 		System.out.println("삭제");
 
 		//Dao -> 삭제
 		int count = guestDao.guestDelete(guestVo);
 		
-		//model담기
-		model.addAttribute("no", guestVo.getNo());
+		//model담기 -> 포워드할때 이용 -> 내부 jsp
+		//param -> 리다이렉트할때 이용 -> 요청 url
+		//model.addAttribute("no", guestVo.getNo());
 		
 		//리턴값이 1이면 삭제, 0이면 다시입력
 		if(count == 1) {
 			return "redirect:/guest/list";
 		} else {
-			return "redirect:/guest/deleteForm";
+			return "redirect:/guest/deleteForm?no=" + guestVo.getNo();
 		}
 
 	}
